f463a6211588be2ed0bba2e699d33ca703ce5674
codegaws <codegaws@gmail.com>
2025-10-27 16:21:23 -0500

    update

---
+++ gadget-plus/src/main/java/com/debugeandoideas/gadgetplus/GadgetPlusApplication.java
@@ -0,0 +1,24 @@
+package com.debugeandoideas.gadgetplus;
+
+import com.debugeandoideas.gadgetplus.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetPlusApplication implements CommandLineRunner {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetPlusApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        this.orderRepository.findAll().forEach(System.out::println);
+    }
+}
