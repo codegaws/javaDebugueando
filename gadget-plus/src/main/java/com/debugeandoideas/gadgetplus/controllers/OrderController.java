49921df3ecdd22f937a0aa7b31cac5d4bc7c90b1
codegaws <codegaws@gmail.com>
2026-01-06 22:07:32 -0500

    Implement delete methods in OrdersCrudService and OrderRepository; enhance OrderController for client name deletion; update documentation on transaction management

--- gadget-plus/src/main/java/com/debugeandoideas/gadgetplus/controllers/OrderController.java
+++ gadget-plus/src/main/java/com/debugeandoideas/gadgetplus/controllers/OrderController.java
@@ -4,6 +4,8 @@ import com.debugeandoideas.gadgetplus.dto.OrderDTO;
 import com.debugeandoideas.gadgetplus.services.OrdersCrudService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -30,4 +32,18 @@ public class OrderController {
     public ResponseEntity<OrderDTO> update(@PathVariable Long id, @RequestBody OrderDTO orderDTO) {
         return ResponseEntity.ok(this.ordersCrudService.update(orderDTO, id));
     }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.ordersCrudService.delete(id);
+        return ResponseEntity.noContent().build();//retornamos un 204 cuando borramos
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteByName(@RequestParam String name) {
+        this.ordersCrudService.delete(name);
+        return ResponseEntity.noContent().build();//retornamos un 204 cuando borramos
+    }
+
+
 }
